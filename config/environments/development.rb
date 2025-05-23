d95d55381ddb47739021da30a11c281aaf6f8f75
Gadde Vinuthna <vinuthna.gadde@cerner.com>
2025-05-23 19:33:51 +0530

    updated development env to configure Redis as cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,13 +23,15 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    # config.cache_store = :memory_store
     config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{2.days.to_i}" }
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # config.cache_store = :null_store
   end
+
+  config.cache_store = :redis_cache_store, { url: 'redis://localhost:6379/1' }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
